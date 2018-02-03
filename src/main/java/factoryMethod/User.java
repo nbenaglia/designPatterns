724697634964f41c511fc54f5578bb6554800ab1
Nicola Benaglia <nico.benaz@gmail.com>
2018-02-03 19:28:44 +0100

    Factory pattern: added static method

--- src/main/java/factoryMethod/User.java
+++ src/main/java/factoryMethod/User.java
@@ -1,14 +1,19 @@
 package factoryMethod;
 
 public class User {
+
+    public static void userWants(FinancialProduct product) {
+        System.out.println("User wants to buy a " + product.name());
+    }
+
     public static void main(String[] args) {
         FinancialProduct loan = FinancialProductFactory.createProduct("loan");
-        System.out.println("User wants to buy a " + loan.name());
+        userWants(loan);
 
         FinancialProduct stock = FinancialProductFactory.createProduct("stock");
-        System.out.println("User wants to buy a " + stock.name());
+        userWants(stock);
 
         FinancialProduct bond = FinancialProductFactory.createProduct("bond");
-        System.out.println("User wants to buy a " + bond.name());
+        userWants(bond);
     }
 }
